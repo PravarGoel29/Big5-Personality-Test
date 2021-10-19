472d8e0bf795258ccd35d06571ca457e44f92989
Pravar Goel <91350539+PravarGoel29@users.noreply.github.com>
2021-10-18 22:42:38 -0400

    Add files via upload

---
+++ AcceptanceTest.py
@@ -0,0 +1,19 @@
+import Project03
+import RefactoredUserStory1
+import KahaniUserStory1
+import MarrbfDeath
+
+Project03.tablefunction()
+
+string1 = 'BIRT'
+string2 = 'DEAT'
+string3 = 'MARR'
+string4 = 'DIV'
+
+print("Birth date and death date records:")
+RefactoredUserStory1.BirthDeathDate(string1,string2,'Birth Date','Death Date')
+print("Marriage date and divorce date records:")
+RefactoredUserStory1.BirthDeathDate(string3,string4,'Maariage Date','Divorce Date')
+print("Records of birth before parents' marriage:")
+KahaniUserStory1.birtbefparmarr()
+
