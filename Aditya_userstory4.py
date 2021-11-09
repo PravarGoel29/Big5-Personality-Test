ea2bf4dfcc4ed18c1a7f6e62cdbfb2f3f0cf40b2
Pravar Goel <91350539+PravarGoel29@users.noreply.github.com>
2021-11-08 22:02:17 -0500

    Add files via upload

---
+++ Aditya_userstory4.py
@@ -0,0 +1,33 @@
+
+def Adityafunction():
+
+    userStoryName('US12')
+    results = get_family()
+
+    for res in results:
+        if ("HUSBAND" or "WIFE") in res:
+            # print("___________"+res["HUSBAND"])
+            # print("___________"+res["WIFE"])
+            husband = res["HUSBAND"]
+            wife = res["WIFE"]
+
+            indi = get_people()
+            for temp in indi:
+                if husband == temp["ID"] and (temp["SEX"] == "F"):
+
+
+
+
+
+                    message = "Gender for Husband ID  " +str(husband ) +", is F, therefore Gender is not valid."
+                    save_invalid_people_for_print(temp["ID"], "US12", message)
+
+                if wife == temp["ID"] and (temp["SEX"] == "M"):
+
+
+
+
+
+
+                    message = "Gender for Wife ID  " +str(wife ) +", is M, therefore Gender is not valid."
+                # save_invalid_people_for_print(temp["ID"], "US12", message)
