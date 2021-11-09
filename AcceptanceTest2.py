d0e66636618628d6dab4aa9e3b7a82e72b0f3734
Pravar Goel <91350539+PravarGoel29@users.noreply.github.com>
2021-11-08 21:11:15 -0500

    Add files via upload

---
+++ AcceptanceTest2.py
@@ -0,0 +1,8 @@
+import DeathBfDivorce
+import DatesBfCurrent
+import Savleen_sprint2
+
+DatesBfCurrent.DivDeathDate()
+DeathBfDivorce.DivDeathDate('DEAT', 'Divorce Date', 'Death Date')
+Savleen_sprint2.convertdate()
+
