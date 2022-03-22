2e1debbd1f3166ad4d9cfaaa41661135743136d6
Pravar Goel <91350539+PravarGoel29@users.noreply.github.com>
2022-03-22 12:42:42 -0400

    Delete AcceptanceTest2.py

--- AcceptanceTest2.py
+++
@@ -1,8 +0,0 @@
-import DeathBfDivorce
-import DatesBfCurrent
-import Savleen_sprint2
-
-DatesBfCurrent.DivDeathDate()
-DeathBfDivorce.DivDeathDate('DEAT', 'Divorce Date', 'Death Date')
-Savleen_sprint2.convertdate()
-
